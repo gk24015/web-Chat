4423fe32cc2db019ce1efb3e72bdba16f4dba44c
Gulshan Kumar <gk24015@gmail.com>
2020-07-27 03:44:48 +0530

    fixed is typing

--- public/clientS.js
+++ public/clientS.js
@@ -32,7 +32,7 @@ message.addEventListener('keypress',() =>{
 socket.on('chat', (data)=>{
     typing.innerHTML="";  //while texting is typing string should be ""
     output.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>'
-    typing.innerHTML="";
+   // typing.innerHTML="";
 })
 
 socket.on('userisTyping',(data)=>{
