5ee3d035ab1d7534e770b0743b47c1cf64a61a2b
Gulshan Kumar <gk24015@gmail.com>
2020-07-27 02:26:00 +0530

    made chat platform

---
+++ public/clientS.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:3000'); //location of where server is hosting socket app
+
+socket.on('chat-message', data =>{
+    console.log(data)
+});
+
+// query DOM
+const message = document.getElementById('message');
+      handle = document.getElementById('handle');
+      button =  document.getElementById('submit');
+      output = document.getElementById('output');
+      typing = document.getElementById('typing');
+
+// Emit events
+
+button.addEventListener('click', () =>
+{
+    socket.emit('chat', {//here chat is variable
+        message: message.value,
+        handle: handle.value
+    })
+    document.getElementById('message').value="";//clearing messege from box once typed
+}) 
+
+//sending the typing messege
+message.addEventListener('keypress',() =>{
+    socket.emit('userisTyping',handle.value)
+  // document.getElementById('message')="";
+} )
+// Listen to events
+// here chat is variable could be anything
+socket.on('chat', (data)=>{
+    typing.innerHTML="";  //while texting is typing string should be ""
+    output.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>'
+    typing.innerHTML="";
+})
+
+socket.on('userisTyping',(data)=>{
+    typing.innerHTML='<p><em>'+data+'isTyping...</em></p>'
+})
---
+++ server.js
@@ -0,0 +1,44 @@
+// io needs to use HTTP, express will still be the middleware for routes
+const express =require ('express')
+const app = express(); //creates the express app
+const httpserver = require('http').createServer(app); //app is an http server
+const io = require('socket.io')(httpserver);
+
+const port = process.env.PORT || 3000;
+
+// http server listening on port
+httpserver.listen(port, function(){
+    console.log('listening on *:3000');
+  });
+
+  app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket)=>{
+    // callback function after connection is made to the client
+
+    // recieves a chat event, then sends the data to other sockets
+    socket.on('chat', (data)=>{
+        io.sockets.emit('chat', data)
+    });
+    socket.on('userisTyping',(data) =>{
+        socket.broadcast.emit('userisTyping',data)
+    })
+});
+
+
+// http server listening on port
+
+
+//   functions for successful connection 
+// function successfulConnection() {
+//     socket.emit('chat-message', 'Hello Welcome to the Chat My Guy!')
+// }
+
+// calls function on load
+// http server object named io is listening to connection events then running the function 
+
+  
